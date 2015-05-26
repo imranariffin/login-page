ccb26a7bffb761af09d1c01236f4f4d24040e6b1
Imran Ariffin <imran.ariffin@mail.utoronto.ca>
2015-05-25 20:36:03 -0400

    added schemas

--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//mongoDB setup
-var mongoose = require('mongoose');
-var Schema = require('../schemas/schemas.js');
-var User = Schema.User;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Auth - login' });
-});
-
-router.post('/', function (req, res) {
-	User.findOne({email : req.body.email}, function (err, user) {
-		//check if user exist
-		if (user) {
-			// check if password given is correct
-			var inputPassword = req.body.password;
-			var dbPassword = user.password;
-			if ( inputPassword === dbPassword) {
-				res.redirect('/dashboard');
-			} else {
-				res.send('Error: wrong password for ' + user.email);
-			}
-		} else {
-			res.send('Error: user does not exist');
-		}
-		res.send();
-	});
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,39 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-// mongodDB setup
-var mongoose = require('mongoose');
-var Schema = require('../schemas/schemas.js');
-var User = Schema.User;
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('register', { title: 'Auth - register' });
 });
 
 router.post('/', function (req, res) {
-	//create new document from req
-	var user = new User({
-		id : req.body.id,
-		firstName : req.body.firstName,
-		lastName : req.body.lastName,
-		email : req.body.email,
-		password : req.body.password
-	});
-	//save if no oredy on db
-	user.save( function (err) {
-		if (err) {
-			var err = "Something bad happened. Try again!";
-			if (err.code === 11000) {
-				err = "Email oredy taken";
-			}
-			res.send("Error: " + err);	
-		} else {
-			console.log("Save success");
-			res.redirect('/dashboard');
-		}
-		res.end();
-	})
+	res.json(req.body);
 });
 
 module.exports = router;
