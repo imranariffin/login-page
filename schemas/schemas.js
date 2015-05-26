741d6bd4a78e2256571958f635b4e77943be90cb
Imran Ariffin <imran.ariffin@mail.utoronto.ca>
2015-05-25 20:11:48 -0400

    added schemas

---
+++ schemas/schemas.js
@@ -0,0 +1,15 @@
+//schemas
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var User = mongoose.model('User', new Schema ({
+	id : ObjectId,
+	firstName : String,
+	lastName : String,
+	email : {type : String, unique : true},
+	password : String
+}));
+
+exports.User = User;
