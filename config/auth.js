17e0f5ddf40cc00d55a728488b33d6e1a9c8cd30
imran <imran.ariffin@mail.utoronto.ca>
2015-07-03 11:34:05 +0800

    changed redirecturl to __dirname + /.bla//

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,12 @@
+var path = require('path');
+
+
+var callbackURL = path.join(__dirname, '/auth/facebook/callback');
+
 module.exports = {
 	'facebookAuth' : {
 		'clientID' : '1620888751493470',
 		'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
-		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL' : callbackURL
 	}
 };
